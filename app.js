741a59161de33ecc4fce897248ef11c5e8ca9c68
Isaac Halvorson <hello@hisaac.net>
2016-11-18 20:18:11 -0600

    create todo route

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
+var todo = require('./routes/todo');
 var port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use('/todo', todo);
 
 app.get('/', index);
 app.use(express.static(__dirname + '/public'));
